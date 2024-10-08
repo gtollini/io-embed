1b2d23508dcf2e503b525c256c99603d35423b4c
Gabriel <gabrieltollini@hotmail.com>
2024-10-07 23:12:05 -0300

    cabal checks

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,7 +13,7 @@ main = hspec $ do
   describe "Testing for all embeddable types except Bytes" $ do
     it "embeddable types" $ do
       $(embedIO $ return 'a') `shouldBe` 'a'
-      $(embedIO $ return ("Hello world!" :: String))   `shouldBe` "Hello world!"
-      $(embedIO $ return (1 :: Integer))   `shouldBe` 1
-      $(embedIO $ return (1 :: Rational))  `shouldBe` 1
+      $(embedIO $ return ("Hello world!" :: String)) `shouldBe` ("Hello world!" :: String)
+      $(embedIO $ return (1 :: Integer))             `shouldBe` (1 :: Integer)
+      $(embedIO $ return (1 :: Rational))            `shouldBe` (1 :: Rational)
       $(embedIO $ return ("Hello binary world!" :: ByteString)) `shouldBe` "Hello binary world!"
